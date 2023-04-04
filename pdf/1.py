570bb2145524f557aae95f028781235dbd4d3237
runfa.li <runfa.li@chainedfinance.com>
2023-04-04 14:30:50 +0800

    new

---
+++ pdf/1.py
@@ -0,0 +1,17 @@
+import requests
+
+headers = {
+    'content-type': 'text/plain;',
+}
+data = '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockcount", "params": [] }'
+response = requests.post('http://10.18.193.199:12345', headers=headers, data=data, auth=('CF', 'ITsupport'))
+print(response.text)
+#一般情况下就可以请求成功得到返回值
+# curl --user CF --data-binary \
+# '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockcount", "params": [] }' \
+# -H 'content-type: text/plain;' http://183.2.185.113:8332
+
+
+# curl --user CF --data-binary \
+# '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockcount", "params": [] }' \
+# -H 'content-type: text/plain;' --resolve 127.0.0.1:80:127.0.0.1 http://127.0.0.1:12345
