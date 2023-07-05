57f2751d22cf96833195cd86200be484bd2b7453
runfa.li <runfa.li@chainedfinance.com>
2023-07-05 17:31:55 +0800

    update

--- SMWEB/login/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    pass
-    # id int primary_key auto_increment
-    id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=32)
-    password = models.IntegerField()
